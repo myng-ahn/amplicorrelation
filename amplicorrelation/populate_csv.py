8f24b0534e803d025bfb5fb9e60e76856a20b74b
myng-ahn <myahn725@gmail.com>
2023-06-06 06:26:38 -0700

    write_html.py

---
+++ amplicorrelation/populate_csv.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import requests
+import json
+import os
+
+BASE_URL = "https://www.genenetwork.nl/api/v1/gene/"
+# Specify the root directory from which to start the iteration
+# root_dir = "/Users/myahn725/Code/CSE_182/project/amplicorrelation/out"
+root_dir = "out"
+df = pd.DataFrame(columns=["sample"])
+
+row_id = []
+columns = {}
+samples = 0
+# Iterate over every subdirectory in the root directory
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    # dirpath: the path to the current subdirectory
+    # dirnames: a list of the subdirectories in dirpath
+    # filenames: a list of the filenames in dirpath
+
+    for amplicons in dirnames:
+        path = os.path.join(dirpath, amplicons)
+
+        for dirpath, dirnames, _ in os.walk(path):
+            for cycles in dirnames:
+                path = os.path.join(dirpath, cycles)
+                for subdirpath, subdirnames, subfilenames in os.walk(path):
+                    for file in subfilenames:
+                        # row_id.append(amplicons + '_' + cycles)
+                        if "genes" in file:
+                            # print(file)
+                            full_path = os.path.join(path, file)
+
+                            print(full_path)
+                            with open(full_path, "r") as f:
+                                # Read all lines of the file into a list
+                                lines = f.readlines()
+                            genes = [line.strip() for line in lines]
+
+                            for gene_id in genes:
+                                print(gene_id)
+                                res = requests.get(f"{BASE_URL}{gene_id}")
+                                if res.status_code != 200:
+                                    print()
+                                    continue
+                                data = res.json()
+                                gene_id_dict = data["gene"]
+                                cycle_genes = []
+                                if (
+                                    gene_id_dict["biotype"] == "ncRNA"
+                                    or gene_id_dict["biotype"] == "lncRNA"
+                                ):
+                                    if gene_id not in df.columns:
+                                        df[gene_id] = pd.Series(dtype=int)
+                                        cycle_genes.append(gene_id)
+
+                            new_row = {"sample": amplicons + "_" + cycles}
+                            for column in df.columns:
+                                if column in cycle_genes:
+                                    new_row[column] = 1
+                                else:
+                                    new_row[column] = 0
+
+                            df = df.append(new_row, ignore_index=True)
+                            # df.loc[amplicons + "_" + cycles] = new_row
+
+
+output_file = "out_matrix.csv"
+
+# Write the DataFrame to a CSV file
+df.to_csv(output_file, index=True)
+
+
+"""
+# Iterate over every subdirectory in the root directory
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    # dirpath: the path to the current subdirectory
+    # dirnames: a list of the subdirectories in dirpath
+    # filenames: a list of the filenames in dirpath
+
+    # Process the current subdirectory as needed
+    print("Subdirectory:", dirpath)
+
+    # Iterate over the filenames in the current subdirectory
+    for path in dirnames:
+        for subdirpath, subdirnames, subfilenames in os.walk(dirnames):
+        # Process each filename in the subdirectory as needed
+            for file in subdirnames:
+                print(file)
+###################################################
+BASE_URL = "https://www.genenetwork.nl/api/v1/gene/"
+
+gene = "TMCC1"
+res = requests.get(f"{BASE_URL}{gene}")
+data = res.json()
+# print(res.status_code)
+# print(res.content)
+# print(data["comment"])
+#print(data.keys())
+print(data["gene"])
+"""
---
+++ amplicorrelation/write_html.py
@@ -0,0 +1,185 @@
+import argparse
+import requests
+import re
+from subprocess import run
+
+# database from which we get info
+BASE_URL = "https://www.genenetwork.nl/api/v1/gene/"
+
+parser = argparse.ArgumentParser(
+    prog="write_html.py", description="Create gene description page"
+)
+parser.add_argument("-s", "--sample_name", required=True, help="Name of sample")
+parser.add_argument(
+    "-a", "--amplicon_number", required=True, type=int, help="Amplicon number"
+)
+parser.add_argument(
+    "-c", "--cycle_number", required=True, type=int, help="Cycle number"
+)
+
+args = parser.parse_args()
+sample_name = args.sample_name
+amplicon_num = args.amplicon_number
+cycle_num = args.cycle_number
+output_dir = f"out/{sample_name}_amplicon{amplicon_num}/cycle{cycle_num}"
+fname = f"{sample_name}_amplicon{amplicon_num}_cycle_{cycle_num}"
+
+known_oncogenes = []
+with open("oncogenes.txt", "r") as f:
+    for l in f:
+        known_oncogenes.append(l.strip())
+
+gene_info = ""
+genes = []
+with open(f"{output_dir}/{sample_name}_amplicon{amplicon_num}_genes.txt", "r") as f:
+    for l in f:
+        genes.append(l.strip())
+for gene in genes:
+    print(f"Fetching information about {gene}...")
+    res = requests.get(f"{BASE_URL}{gene}")
+    if res.status_code == 200:
+        data = res.json()
+        biotype = data["gene"]["biotype"]
+        description = data["gene"]["description"]
+        # remove '[Source:HGNC Symbol;Acc:HGNC:31098]' line from description
+        source_string = "\[Source:.*\]"
+        description = re.sub(source_string, "", description)
+        oncogene_class = "oncogeneBtn" if gene in known_oncogenes else ""
+        gene_info += f"""
+            <button class="geneInfoBtn {oncogene_class}">{gene}</button>
+            <div class="panel">
+                <p>Biotype: {biotype}</p>
+                <p>Description: {description}</p>
+            </div>
+        """
+    else:
+        print(f"Unable to get information for {gene}")
+if gene_info == "":
+    gene_info = "Unable to retrieve gene information"
+
+css = r"""
+html {
+    height: 100vh;
+    padding: 0;
+    font-family: Helvetica, sans-serif;
+}
+body {
+    height: 100%;
+    padding: 0;
+}
+
+img {
+    max-width:100%;
+    max-height:100%;
+}
+
+.wrapper {
+    justify-content: center;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10vh;
+    text-align: center;
+}
+
+.split {
+    display: grid;
+    grid-template-columns: 2fr 3fr;
+    margin-top:6vh
+}
+
+.split > * {
+    padding:1rem;
+}
+
+.geneInfoWrapper {
+    margin-left: 10%
+}
+
+.geneInfoBtn {
+    cursor: pointer;
+    width: 100%;
+    padding: 3% 5% 3%;
+    text-align: left;
+    font-size: 2.5em;
+    border: none;
+    outline: none;
+    transition: 0.4s;
+}
+
+.oncogeneBtn {
+    color: #a80402;
+}
+
+.cycleViz {
+    position: fixed;
+    width: 50vw;
+    left: 45%;
+    top: 15%;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .geneInfoBtn:hover {
+    background-color: #ccc;
+}
+
+.panel {
+    padding: 0 18px;
+    background-color: white;
+    text-align: left;
+    font-size: 1.5em;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.2s ease-out;
+}
+
+"""
+with open(f"{output_dir}/style.css", "w") as f:
+    f.write(css)
+
+script = r"""
+    <script>
+        var acc = document.getElementsByClassName("geneInfoBtn");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            });
+        }
+    </script>
+"""
+
+html = f"""
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>{fname} gene description</title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+
+    <body>
+        <div class="wrapper">
+        <h1>{fname} genes description</h1>
+        <div class="split">
+            <div class="geneInfoWrapper">
+                {gene_info}
+            </div>
+            <div><img class="cycleViz" src="{fname}.png" /></div>
+            </div>
+        </div>
+        {script}
+    </body>
+</html>
+"""
+with open(f"{output_dir}/{fname}.html", "w") as f:
+    f.write(html)
