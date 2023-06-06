1834e5be027a9b76d7e9875530fa93ad33ccc004
jjjoelle <jfaybish@ucsd.edu>
2023-06-06 09:59:54 -0700

    make pca

--- amplicorrelation/populate_csv.py
+++ amplicorrelation/populate_csv.py
@@ -7,7 +7,8 @@ import os
 BASE_URL = "https://www.genenetwork.nl/api/v1/gene/"
 # Specify the root directory from which to start the iteration
 # root_dir = "/Users/myahn725/Code/CSE_182/project/amplicorrelation/out"
-root_dir = "out"
+root_dir = "/Users/jfaybishenko/Documents/School/CSE/CSE_182/project/amplicorrelation/out"
+output_file = "out_matrix.csv"
 df = pd.DataFrame(columns=["sample"])
 
 row_id = []
@@ -20,50 +21,57 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
     # filenames: a list of the filenames in dirpath
 
     for amplicons in dirnames:
-        path = os.path.join(dirpath, amplicons)
-
-        for dirpath, dirnames, _ in os.walk(path):
-            for cycles in dirnames:
-                path = os.path.join(dirpath, cycles)
-                for subdirpath, subdirnames, subfilenames in os.walk(path):
+        old_path = os.path.join(dirpath, amplicons)
+        df.to_csv(output_file, index=True)
+        for subdirpath, subdirnames, _ in os.walk(old_path):
+            for cycles in subdirnames:
+                path = os.path.join(old_path, cycles)
+                
+                for subsubdirpath, subsubdirnames, subfilenames in os.walk(path):
                     for file in subfilenames:
                         # row_id.append(amplicons + '_' + cycles)
+                        
                         if "genes" in file:
                             # print(file)
                             full_path = os.path.join(path, file)
 
-                            print(full_path)
+                            
                             with open(full_path, "r") as f:
                                 # Read all lines of the file into a list
                                 lines = f.readlines()
                             genes = [line.strip() for line in lines]
-
+                            cycle_genes = []
                             for gene_id in genes:
-                                print(gene_id)
-                                res = requests.get(f"{BASE_URL}{gene_id}")
-                                data = res.json()
+                                try:
+                                    res = requests.get(f"{BASE_URL}{gene_id}")
+                                    data = res.json()
+                                except:
+                                    print("Gene Broke:")
+                                    print(gene_id, cycles, amplicons)
+                                    continue
+                                if res.status_code != 200:
+                                    continue
                                 gene_id_dict = data["gene"]
-                                cycle_genes = []
+                                
                                 if (
-                                    gene_id_dict["biotype"] == "ncRNA"
-                                    or gene_id_dict["biotype"] == "lncRNA"
+                                    gene_id_dict["biotype"] != "protein_coding"
                                 ):
+                                    #print(amplicons, cycles, gene_id)
                                     if gene_id not in df.columns:
                                         df[gene_id] = pd.Series(dtype=int)
                                         cycle_genes.append(gene_id)
-
-                            new_row = {"sample": amplicons + "_" + cycles}
+                            identity = str(amplicons) + "_" + str(cycles)
+                            new_row = {"sample": identity}
+                            
                             for column in df.columns:
                                 if column in cycle_genes:
                                     new_row[column] = 1
                                 else:
                                     new_row[column] = 0
 
-                            df = df.append(new_row, ignore_index=True)
-                            # df.loc[amplicons + "_" + cycles] = new_row
+                            #df = df.append(new_row, ignore_index=True)
+                            df.loc[identity] = new_row
 
-
-output_file = "out_matrix.csv"
 
 # Write the DataFrame to a CSV file
 df.to_csv(output_file, index=True)
